f998de3ec6aebc842076aa9e1c2168795f3ef316
Tanya Ramirez <tcramire@gmail.com>
2017-05-16 22:55:59 -0400

    4 funciones funcionando ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
-function llamarBoton(){ // ver por que funciona al 2do clic???
+function llamarBoton(){ 
 	var boton = document.getElementById("demo");
 	boton.addEventListener("click", function(){
 		alert("Hola mundo!");
 	})
 };
+
 
 //FUNCION ONLOAD (en elemento body)
 
@@ -11,28 +12,37 @@ function llamarBoton(){ // ver por que funciona al 2do clic???
 		alert("Loading...");
 	};
 
-//FUNCION ONMOUSEOUT (en elemento input)
+/*
+PROBAR SI FUNCIONA DE ESTA MANERA
+
+window.onload = function(){
+		document.getElementById("demo").addEventListener("onload", );
+}
+*/
+
+//FUNCION ONMOUSEOVER (en elemento input)
 
 function sobre(){
-	var sobre = document.getElementById("soyUnInput");
-	sobre.addEventListener("onmouseout", function(){
+	var sobre = document.getElementById("unInput");
+	sobre.addEventListener("mouseover", function(){
 		alert("Estas sobre mi");
 	})
 };
 
 
 
+//FUNCION ONMOUSEOUT (en elemento input)
 
-	
+	function fuera(){
+	var fuera= document.getElementById("unInput");
+	fuera.addEventListener("mouseout", function(){
+		alert("No estas sobre mi");
+	})
+};
 
-//FUNCION ONMOUSEOVER (en elemento input)
 
 
-/*
-PROBAR SI FUNCIONA DE ESTA MANERA
 
-window.onload = function(){
-		document.getElementById("demo").addEventListener("click", );
-}
-*/
+
+
 
